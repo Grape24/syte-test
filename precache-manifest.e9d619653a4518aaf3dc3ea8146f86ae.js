ea593e01e55979b83a60b56affda5eba500881d6
Enav <eavs772@gmail.com>
2020-09-19 19:58:30 +0300

    Updates

---
+++ precache-manifest.e9d619653a4518aaf3dc3ea8146f86ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcbf94dc28b0694370a21c91c8b47cb",
+    "url": "/syte-test/index.html"
+  },
+  {
+    "revision": "fda0e475d5c8c20a274a",
+    "url": "/syte-test/static/css/main.e7bcde44.chunk.css"
+  },
+  {
+    "revision": "d6597dba7b7b9b48bde3",
+    "url": "/syte-test/static/js/2.7e63df61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/syte-test/static/js/2.7e63df61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda0e475d5c8c20a274a",
+    "url": "/syte-test/static/js/main.e256a5f6.chunk.js"
+  },
+  {
+    "revision": "3e27402a9b7320391da8",
+    "url": "/syte-test/static/js/runtime-main.9fa94d35.js"
+  },
+  {
+    "revision": "3bc15b8781be905edf8b75e5196c8408",
+    "url": "/syte-test/static/media/like.3bc15b87.svg"
+  },
+  {
+    "revision": "e565c18a346c2915993ca47eb7ba8ca7",
+    "url": "/syte-test/static/media/likeActive.e565c18a.svg"
+  },
+  {
+    "revision": "16ce311fc48ebb98a1dbb8fd5dc34f3c",
+    "url": "/syte-test/static/media/loader.16ce311f.svg"
+  }
+]);
